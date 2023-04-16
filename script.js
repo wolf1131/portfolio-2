caf2596e1242f4bcde869d103d25a48a0bc00af8
wolf1131 <adinamber1883@gmail.com>
2023-04-16 13:39:00 +0300

    mailjs-projects

--- script.js
+++ script.js
@@ -42,6 +42,36 @@ menuIcon.classList.remove("bx-x");
 navbar.classList.remove("active");
 };
 
+// Email js
+const contactForm = document.querySelector('#contact form'),
+      contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_tvrgkqh", "template_agi1kjx", contactForm, "aYuZwdcqznqt1z5Iu")
+        .then(() => {
+            // show success message
+            contactMessage.textContent = "Message sent successfully ✅";
+            
+            // remove success message after 5 seconds
+            setTimeout(() => {
+                contactMessage.textContent = "";
+            }, 5000);
+            
+            // clear input fields
+            contactForm.reset();
+        })
+        .catch(() => {
+            // show error message
+            contactMessage.textContent = "Message not sent (service error) ❌";
+        });
+};
+
+contactForm.addEventListener("submit", sendEmail);
+
+
+
 // scroll reveal
 ScrollReveal({
     reset: true,
